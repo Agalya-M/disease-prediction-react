7320dc300846e13c5ae6e00b6e99e2f0ade283a2
Agalya-M <agalya12172001@gmail.com>
2023-04-22 20:30:31 +0530

    firebase for breast cancer test

--- src/Screens/breastCancer/index.jsx
+++ src/Screens/breastCancer/index.jsx
@@ -3,9 +3,17 @@ import GroupInput from "../../Components/groupInput";
 import NavBar from "../../Components/Navbar";
 import "../../Styles/globelStyles.css";
 import { breastCancerInputs } from "../../Utils/utils";
+import { useLocation, useNavigate } from "react-router-dom";
+import { saveDiseaseTestData } from "../../Firebase";
+import ResultCard from "../../Components/resultCard";
 
 const BreastCancerDisease = () => {
-  const [breastCancerValues, setValues] = useState({
+  const location = useLocation();
+  const navigate = useNavigate();
+  const patientDetails = location.state.patientDetails;
+  const diseaseDetails = location.state.diseaseDetails;
+
+  const initialBreastCancerValues = {
     radius_mean: "",
     texture_mean: "",
     perimeter_mean: "",
@@ -32,15 +40,41 @@ const BreastCancerDisease = () => {
     concave_points_worst: "",
     symmetry_worst: "",
     fractal_dimension_worst: "",
-  });
+  };
+
+  const [breastCancerValues, setValues] = useState(initialBreastCancerValues);
+  const [loader, setLoader] = useState(false);
+  const [showResults, setShowResults] = useState(false);
+  const [result, setResult] = useState(0);
 
   const onChange = (inputName, value) => {
-    console.log(inputName, value);
     setValues({ ...breastCancerValues, [inputName]: value });
   };
 
   const onSubmit = () => {
-    console.log({ breastCancerValues });
+    setLoader(true);
+    setTimeout(() => {
+      setShowResults(true);
+      setLoader(false);
+      setResult(random(0, 1));
+    }, 2000);
+  };
+
+  const onSave = async () => {
+    setShowResults(false);
+    setLoader(true);
+    const payload = {
+      patientDiseaseInputValues: breastCancerValues,
+      ...patientDetails,
+      ...diseaseDetails,
+      createdAt: new Date(),
+      result,
+    };
+    console.log(payload);
+    await saveDiseaseTestData(payload);
+    setLoader(false);
+    setValues(initialBreastCancerValues);
+    navigate("/newTest");
   };
 
   return (
@@ -51,12 +85,21 @@ const BreastCancerDisease = () => {
           <h1>Breast Cancer Prediction</h1>
         </div>
         <div className="input-wrapper">
-          <GroupInput
-            data={breastCancerInputs}
-            dataValue={breastCancerValues}
-            onChange={onChange}
-            onSubmit={onSubmit}
-          />
+          {!showResults ? (
+            <GroupInput
+              data={breastCancerInputs}
+              dataValue={breastCancerValues}
+              onChange={onChange}
+              onSubmit={onSubmit}
+              isLoading={loader}
+            />
+          ) : (
+            <ResultCard
+              title={"Breast Cancer"}
+              result={result}
+              callback={onSave}
+            />
+          )}
         </div>
       </div>
     </div>
